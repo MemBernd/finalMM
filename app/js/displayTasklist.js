7761d824b66203d6461580dada1f8ec905cb61f9
Vasilis Ch <vascha@kth.se>
2017-10-10 20:25:32 +0200

    Table format for managers.html

--- app/js/displayTasklist.js
+++ app/js/displayTasklist.js
@@ -58,28 +58,29 @@ function alertContents() {
         if (httpRequest.status === 200) {
             var response = JSON.parse(httpRequest.responseText);
             tasklist = response.tasklist;
-            var table="<tr><th>idTask</th><th>Subject</th><th>Description</th><th>Priority</th><th>Status</th><th>EventRecord</th><th>Creator</th><th>Assignee</th></tr>";
-
+            var table="<thead><tr><th class="text-left">idTask</th><th class="text-left">Subject</th><th class="text-left">Description</th><th class="text-left">Priority</th><th class="text-left">Status</th><th class="text-left">EventRecord</th><th class="text-left">Creator</th><th class="text-left">Assignee</th></tr></thead>";
+            table += "<tbody class="table-hover">"
             for (var i = 0; i<tasklist.length; i++){
             	if (tasklist.status[i] == "created" || tasklist.status[i] == "acceptedByAM" && sessionStorage.actor == "janet") {
-            		table += "<tr><td>" + tasklist.idTask[i] + "</td><td>" + tasklist.subject[i] + "</td><td>" + tasklist.description[i] + "</td><td>" + tasklist.priority[i] + "</td><td>" + tasklist.status[i] + "</td><td>" + tasklist.eventRecord[i] + "</td><td>" + tasklist.creator[i] + "</td><td>" + tasklist.assignee[i] + "</td><tr>";
+            		table += "<tr><td class="text-left">" + tasklist.idTask[i] + "</td><td class="text-left">" + tasklist.subject[i] + "</td><td class="text-left">" + tasklist.description[i] + "</td><td class="text-left">" + tasklist.priority[i] + "</td><td class="text-left">" + tasklist.status[i] + "</td><td class="text-left">" + tasklist.eventRecord[i] + "</td><td class="text-left">" + tasklist.creator[i] + "</td><td class="text-left">" + tasklist.assignee[i] + "</td></tr>";
             	}
             	if ((tasklist.status[i] == "acceptedBySCSO" || tasklist.status[i] == "pendingRequest") && sessionStorage.actor == "alice") {
-            		table += "<tr><td>" + tasklist.idTask[i] + "</td><td>" + tasklist.subject[i] + "</td><td>" + tasklist.description[i] + "</td><td>" + tasklist.priority[i] + "</td><td>" + tasklist.status[i] + "</td><td>" + tasklist.eventRecord[i] + "</td><td>" + tasklist.creator[i] + "</td><td>" + tasklist.assignee[i] + "</td><tr>";
+            		table += "<tr><td class="text-left">" + tasklist.idTask[i] + "</td><td class="text-left">" + tasklist.subject[i] + "</td><td class="text-left">" + tasklist.description[i] + "</td><td class="text-left">" + tasklist.priority[i] + "</td><td class="text-left">" + tasklist.status[i] + "</td><td class="text-left">" + tasklist.eventRecord[i] + "</td><td class="text-left">" + tasklist.creator[i] + "</td><td class="text-left">" + tasklist.assignee[i] + "</td></tr>";
             	}
             	if (tasklist.status[i] == "processedByFM" && sessionStorage.actor == "mike") {
-            		table += "<tr><td>" + tasklist.idTask[i] + "</td><td>" + tasklist.subject[i] + "</td><td>" + tasklist.description[i] + "</td><td>" + tasklist.priority[i] + "</td><td>" + tasklist.status[i] + "</td><td>" + tasklist.eventRecord[i] + "</td><td>" + tasklist.creator[i] + "</td><td>" + tasklist.assignee[i] + "</td><tr>";
+            		table += "<tr><td class="text-left">" + tasklist.idTask[i] + "</td><td class="text-left">" + tasklist.subject[i] + "</td><td class="text-left">" + tasklist.description[i] + "</td><td class="text-left">" + tasklist.priority[i] + "</td><td class="text-left">" + tasklist.status[i] + "</td><td class="text-left">" + tasklist.eventRecord[i] + "</td><td class="text-left">" + tasklist.creator[i] + "</td><td class="text-left">" + tasklist.assignee[i] + "</td></tr>";
             	}
             	if ((tasklist.status[i] == "summaryCreated" || tasklist.status[i] == "open" || tasklist.status[i] == "pendingReviewSTM") && (sessionStorage.actor == "jack" || sessionStorage.actor == "natalie")) {
-            		table += "<tr><td>" + tasklist.idTask[i] + "</td><td>" + tasklist.subject[i] + "</td><td>" + tasklist.description[i] + "</td><td>" + tasklist.priority[i] + "</td><td>" + tasklist.status[i] + "</td><td>" + tasklist.eventRecord[i] + "</td><td>" + tasklist.creator[i] + "</td><td>" + tasklist.assignee[i] + "</td><tr>";
+            		table += "<tr><td class="text-left">" + tasklist.idTask[i] + "</td><td class="text-left">" + tasklist.subject[i] + "</td><td class="text-left">" + tasklist.description[i] + "</td><td class="text-left">" + tasklist.priority[i] + "</td><td class="text-left">" + tasklist.status[i] + "</td><td class="text-left">" + tasklist.eventRecord[i] + "</td><td class="text-left">" + tasklist.creator[i] + "</td><td class="text-left">" + tasklist.assignee[i] + "</td></tr>";
             	}
             	if (tasklist.status[i] == "staffRequest" && sessionStorage.actor == "simon") {
-            		table += "<tr><td>" + tasklist.idTask[i] + "</td><td>" + tasklist.subject[i] + "</td><td>" + tasklist.description[i] + "</td><td>" + tasklist.priority[i] + "</td><td>" + tasklist.status[i] + "</td><td>" + tasklist.eventRecord[i] + "</td><td>" + tasklist.creator[i] + "</td><td>" + tasklist.assignee[i] + "</td><tr>";
+            		table += "<tr><td class="text-left">" + tasklist.idTask[i] + "</td><td class="text-left">" + tasklist.subject[i] + "</td><td class="text-left">" + tasklist.description[i] + "</td><td class="text-left">" + tasklist.priority[i] + "</td><td class="text-left">" + tasklist.status[i] + "</td><td class="text-left">" + tasklist.eventRecord[i] + "</td><td class="text-left">" + tasklist.creator[i] + "</td><td class="text-left">" + tasklist.assignee[i] + "</td></tr>";
             	}
             	    
             }
 
-            document.getElementById("listTitle").innerHTML = "<h2>List with tasks</h2><br><br>";
+            table += "/tbody";
+            document.getElementById("listTitle").innerHTML = "<h3>List with tasks</h3>";
             document.getElementById("displaytasks").innerHTML = table;
         }
     } else {
