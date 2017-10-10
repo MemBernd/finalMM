b85581f9920578c50bde86d8f6fe163e9736949a
Bernardo <31217674+MemBernd@users.noreply.github.com>
2017-10-10 16:03:25 +0200

    Seperation of connection file in OwnExample
    Seperated the connection part to be able to reuse it. Additionally, added Project presentation PDF.

--- testing/ownExample/getCredentials.php
+++ testing/ownExample/getCredentials.php
@@ -1,14 +1,9 @@
 
 <?php
+include 'connection.php';
 $name = (isset($_POST['userName'])) ? $_POST['userName'] : 'no name';
-$con = mysqli_connect('localhost','root','','sep');
-if (!$con) {
-    die('Could not connect: ' . mysqli_error($con));
-}
-
-mysqli_select_db($con,"sep");
 $sql="SELECT * FROM employee WHERE username = '".$name."'";
-$result = mysqli_query($con,$sql);
+$result = select($sql); //function is called from connection.php
 $row = mysqli_fetch_assoc($result);
 $array = ['password' => $row['password'], 'role' => $row['role']];
 echo json_encode($array);
