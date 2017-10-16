6cf276462cf3d9d51af1b25c14e825784ee320a9
Vasilis Ch <vascha@kth.se>
2017-10-16 14:02:38 +0200

    UI changes, added new event request for sarah

--- app/js/checklogin.js
+++ app/js/checklogin.js
@@ -7,7 +7,13 @@ function isActorInside(){
 		document.getElementById("islogin").innerHTML = "Logout";
 		document.getElementById("islogin").href = "logout.html"
 		if (sessionStorage.actor != "sarah"){
+			document.getElementById("isTaskOn").innerHTML = "Tasklist"
 			document.getElementById("isTaskOn").style.display = 'block';
+			document.getElementById("isTaskOn").href = "tasklist.html";
+		} else if (sessionStorage.actor == "sarah") {
+			document.getElementById("isTaskOn").innerHTML = "New Event Request";
+			document.getElementById("isTaskOn").style.display = 'block';
+			document.getElementById("isTaskOn").href = "cso.html";
 		}
 	} else {
 		document.getElementById("islogin").innerHTML = "Login";
