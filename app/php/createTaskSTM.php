317cc0897c853cb406230ab3d03d7f069aa0ec52
Bernardo <31217674+MemBernd@users.noreply.github.com>
2017-10-16 10:19:22 +0200

    createTaskSTM correction
    removed echo of sql insert.

--- app/php/createTaskSTM.php
+++ app/php/createTaskSTM.php
@@ -3,7 +3,7 @@ require_once 'connection.php';
 
 $idTask = (isset($_POST['idTask'])) ? $_POST['idTask'] : 0;
 $description = (isset($_POST['description'])) ? $_POST['description'] : "No description provided";
-$assignee = (isset($_POST['assignee'])) ? $_POST['assignee'] : 'no name given';
+$assignee = (isset($_POST['assignee'])) ? $_POST['assignee'] : 'no username';
 $priority = (isset($_POST['priority'])) ? $_POST['priority'] : 'medium';
 
 echo json_encode(createTask($idTask, $description, $assignee, $priority));
@@ -20,7 +20,6 @@ function createTask($idTask, $description, $assignee, $priority) {
         $sql = "Insert into task (subject, description, priority, eventRecord, creator, assignee, statusId) "
         ." Values ('".$subject."', '".$description."', '".$priority."', ".$r['eventRecord'].", '".$r['assignee']."', "
         ." '".$assignee."', ".$statusId.");";
-        echo $sql;
         $result = modify($sql);
         if($result != -1) {
             $return = 'success';
